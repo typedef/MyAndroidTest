41298a8a149ca6a65af5036c8da23e46134d0cdd
zouying <llin.zou@gmail.com>
2013-03-21 21:23:43 +0800

    google map
    google map test

---
+++ src/com/example/myandroidtest/mapdemo.java
@@ -0,0 +1,21 @@
+package com.example.myandroidtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zouying
+ * Date: 13-3-21
+ * Time: 下午5:27
+ * To change this template use File | Settings | File Templates.
+ */
+public class mapdemo extends Activity {
+
+        @Override
+        protected  void onCreate(Bundle bundle){
+            super.onCreate(bundle);
+            setContentView(R.layout.googlemap);
+        }
+}
